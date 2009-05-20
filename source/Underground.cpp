48a78ce3f2cc400d1795b7450f93e9d7002756f7
Will Winder <wwinder.unh@gmail.com>
2009-05-20 20:10:36 +0000

    Moved the constructor and initializer from the header file to the cpp file.
    git-svn-id: http://dsants.googlecode.com/svn/trunk@57 1d7a1664-3ac9-11de-b438-597f59cd7555

--- source/Underground.cpp
+++ source/Underground.cpp
@@ -1,2 +1,13 @@
 #include "Underground.h"
 
+Underground::Underground()
+{
+	tunnelSize = 60;
+	setCenter(0, 0);
+	initGrid();
+}
+
+void Underground::initGrid()
+{
+	getGrid()->generateNest(tunnelSize);
+}
--- source/Underground.h
+++ source/Underground.h
@@ -13,16 +13,18 @@ class Underground: public MapDraw
 		// TODO: this will need to be slightly restructured to allow multiple nests
 		//				so that a huge chunk of memory for multiple "Grid's" will not
 		//				be needed.
-		Underground(){	tunnelSize = 60;
-										setCenter(0, 0);
-										initGrid();
-									}
+//		Underground(){	tunnelSize = 60;
+//										setCenter(0, 0);
+//										initGrid();
+//									}
+		Underground();
 		virtual ~Underground(){ /* superclass destructure deletes */}
 
-		virtual void initGrid()
-		{
-			getGrid()->generateNest(tunnelSize);
-		}
+		virtual void initGrid();
+//		virtual void initGrid()
+//		{
+//			getGrid()->generateNest(tunnelSize);
+//		}
 
 		// Adds an ant in a random location.
 		void addAnt(Ant *a);
